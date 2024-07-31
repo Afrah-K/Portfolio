58a38806abb7b2ad6590aa6202c848d0994cbf0e
Afrah <79169064+Afrah-K@users.noreply.github.com>
2024-07-31 11:55:07 +0000

    Add more projects + change links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             href="/rss"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7">blog</p>
           </a>
         </li>
         <li>
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/Afrah-K/portfolio"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,8 +7,8 @@ const navItems = {
   '/projects': {
     name: 'projects',
   },
-  '/resume': {
-    name: 'resume',
+  '/academia': {
+    name: 'academia',
   },
   '/contact': {
     name: 'contact',
